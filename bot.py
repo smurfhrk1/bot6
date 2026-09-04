a10f790904384fb942799c633856cca4fb1b4cd3
smurfhrk1 <smurfhrk1@gmail.com>
2026-09-04 08:51:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ async def main(nama, email, c):
         await page.check("input.form-check-input")
         
         await page.wait_for_timeout(1000)
-        if(c==0 or True):
+        if(c==0):
             await page.screenshot(path=f"{c}_0reg.png")
         # 3. Regis
         await page.click("button[type='submit']")
@@ -74,11 +74,11 @@ async def main(nama, email, c):
         await page.wait_for_timeout(2000)
         await page.keyboard.press("Enter")
         await page.wait_for_timeout(2000)
-        await page.mouse.click(170, 662)
+        await page.mouse.click(540, 662)
         await page.wait_for_timeout(2000)
-        await page.mouse.click(170, 662)
+        await page.mouse.click(540, 662)
         await page.wait_for_timeout(2000)
-        if(c==0 or True):
+        if(c==0):
             await page.screenshot(path=f"{c}_7booth.png")
 
         print(f"Akun : ({c}) {nama} | Selesai")
